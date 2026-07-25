ece3fc6ba4cd885617517c30ef516754382bd1c9
yladraoui <yladraoui@gmail.com>
2026-07-25 03:15:13 +0100

    feat(auth): fix registration endpoint, context-path and DTO serialization
    - Add @NoArgsConstructor and @AllArgsConstructor to RegistrationRequest to allow Jackson deserialization
    - Resolve context-path routing issues for security filter chain
    - Ensure email notification is properly sent via MailDev upon registration

--- book-network/src/main/java/com/yladraoui/book/BookNetworkApplication.java
+++ book-network/src/main/java/com/yladraoui/book/BookNetworkApplication.java
@@ -1,7 +1,11 @@
 package com.yladraoui.book;
 
 
+import com.yladraoui.book.role.Role;
+import com.yladraoui.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -15,5 +19,14 @@ public class BookNetworkApplication {
 	public static void main(String[] args) {
 		run(BookNetworkApplication.class, args);
 	}
-
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+          if (roleRepository.findByName("USER").isEmpty()) {
+              roleRepository.save(
+                      Role.builder().name("USER").build()
+              );
+          }
+        };
+    }
 }
--- book-network/src/main/java/com/yladraoui/book/auth/RegistrationRequest.java
+++ book-network/src/main/java/com/yladraoui/book/auth/RegistrationRequest.java
@@ -5,15 +5,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationRequest {
     @NotEmpty(message = "Firstname is mandatory")
     @NotBlank(message = "Firstname is mandatory")
