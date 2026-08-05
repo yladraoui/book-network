bfde84cc77e673e08a045b91ec6b44154a9c86f6
yladraoui <yladraoui@gmail.com>
2026-08-05 02:20:39 +0100

    style(architecture): setup project folder structure and openapi services
    - Create core directory for guards, interceptors, and application services
    - Create features directory for auth, book, and feedback modules
    - Create shared directory for reusable components
    - Integrate auto-generated OpenAPI services and models from Swagger specification
    - Adjust project layout to match Angular standards

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideApiConfiguration } from './services/api-configuration';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApiConfiguration('http://localhost:8088/api/v1')
   ]
 };
