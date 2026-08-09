8e0736a39a148072420de671f1b7e08780b3a2c2
yladraoui <yladraoui@gmail.com>
2026-08-09 19:27:53 +0100

    feat(shared): implement menu component
    - Add responsive navigation navbar in shared/components/menu
    - Include links for book features and logout action
    - Integrate TokenService for user session management

--- book-network-ui/src/app/app.ts
+++ book-network-ui/src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Menu } from './shared/components/menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Menu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('book-network-ui');
+  private router = inject(Router);
+
+  get isAuthPage(): boolean {
+    return (this.router.url.includes('/login') || this.router.url.includes('/registe') );
+  }
 }
---
+++ book-network-ui/src/app/shared/components/menu/menu.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { TokenService } from '../../../core/services/token/token.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss',
+})
+export class Menu {
+private tokenService = inject(TokenService);
+  private router = inject(Router);
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/auth/login']);
+  }
+}
